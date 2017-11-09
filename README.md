f7b3fa359bff2e9acf57d45d9c4348c6ef1650a6
Délita Makanda <delita.makanda@gmail.com>
2017-11-09 23:27:43 +0100

    add bank app

--- README.md
+++ README.md
@@ -10,3 +10,6 @@ npm run start
 ```
 python3 manage.py runserver
 ```
+
+## Resources
+* [Webpack Docs](http://owaislone.org/blog/webpack-plus-reactjs-and-django/)
---
+++ bank/models.py
@@ -0,0 +1,64 @@
+import uuid
+from django.conf import settings
+from django.db import models
+
+# Create your models here.
+class Account(models.Model):
+    class Meta:
+        verbose_name = 'Account'
+        verbose_name_plural = 'Accounts'
+
+    MAX_TOTAL_BALANCES = 1000000
+    MAX_BALANCE = 10000
+    MIN_BALANCE = 0
+
+    MAX_DEPOSIT = 1000
+    MIN_DEPOSIT = 1
+
+    MAX_WITHDRAW = 1000
+    MIN_WITHDRAW = 1
+
+    id = models.AutoField(primary_key=True)
+    uid = models.UUIDField(unique=True,editable=False,default=uuid.uuid4, verbose_name='Public identifier')
+    user = models.OneToOneField(settings.AUTH_USER_MODEL, on_delete=models.PROTECT)
+    created = models.DateTimeField(blank=True)
+    modified = models.DateTimeField(blank=True)
+    balance = models.PositiveIntegerField(verbose_name='Current balance')
+
+
+class Action(models.Model):
+    class Meta:
+        verbose_name = 'Account action'
+        verbose_name_plural = 'Account actions'
+
+    ACTION_TYPE_CREATED = 'CREATED'
+    ACTION_TYPE_DEPOSITED = 'DEPOSITED'
+    ACTION_TYPE_WITHDRAWN = 'WITHDRAWN'
+    ACTION_TYPE_CHOICES = (
+        (ACTION_TYPE_CREATED, 'Created'),
+        (ACTION_TYPE_DEPOSITED, 'Deposited'),
+        (ACTION_TYPE_WITHDRAWN, 'Withdrawn'),
+    )
+
+    REFERENCE_TYPE_BANK_TRANSFER = 'BANK_TRANSFER'
+    REFERENCE_TYPE_CHECK = 'CHECK'
+    REFERENCE_TYPE_CASH = 'CASH'
+    REFRENCE_TYPE_NONE = 'NONE'
+    REFERENCE_TYPE_CHOICES = (
+        (REFERENCE_TYPE_BANK_TRANSFER, 'Bank transfer'),
+        (REFERENCE_TYPE_CHECK, 'Check'),
+        (REFERENCE_TYPE_CASH, 'Cash'),
+        (REFRENCE_TYPE_NONE, 'None'),
+    )
+
+    id = models.AutoField(primary_key=True)
+    user_friendly_id = models.UUIDField(unique=True,editable=False,max_length=30)
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.PROTECT, help_text='User on make an action')
+    created = models.DateTimeField(blank=True)
+    account = models.ForeignKey(Account)
+    type = models.CharField(max_length=30,choices=ACTION_TYPE_CHOICES)
+    delta = models.IntegerField(help_text='Balance delta.')
+    reference = models.TextField(blank=True)
+    reference_type = models.CharField(max_length=30, choices=REFERENCE_TYPE_CHOICES, default=REFRENCE_TYPE_NONE)
+    comment = models.TextField(blank=True)
+    debug_balance = models.IntegerField(help_text='Balance after action')
