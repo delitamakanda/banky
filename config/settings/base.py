e5acf64bf2e71f62a9de4bcf88b65ec62e833ee3
Délita Makanda <delita.makanda@gmail.com>
2018-03-25 21:17:49 +0200

    change setting

--- config/settings/base.py
+++ config/settings/base.py
@@ -154,5 +154,9 @@ if not DEBUG:
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
+    ),
+    'DEFAULT_PERMISSION_CLASSES': (
+        'rest_framework.permissions.IsAuthenticated',
     )
 }
--- config/urls.py
+++ config/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.conf.urls import url, include
 from django.contrib import admin
 from django.views.generic.base import TemplateView
+from django.views.decorators.csrf import csrf_exempt
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
     url(r'^api/', include('api.urls')),
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', csrf_exempt(TemplateView.as_view(template_name='index.html'))),
 ]
