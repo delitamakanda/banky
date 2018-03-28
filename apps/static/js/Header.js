0a6d30f12d907c456934d06eb5b2945f21925359
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-03-28 17:28:55 +0200

    Create Header.js

---
+++ apps/static/js/Header.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+import './App.css';
+
+
+class Header extends Component {
+
+    render() {
+
+        return (
+            <div>
+                title 1
+            </div>
+        );
+    }
+}
+
+export default Header;
