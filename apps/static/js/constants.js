63837df3840411c82d345cf9e03548f45e5875f6
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-03-29 12:09:47 +0200

    fluxtify constant

--- apps/static/js/constants.js
+++ apps/static/js/constants.js
@@ -1,5 +1,8 @@
 export default {
     CREATED_ACCOUNT: 'created account',
     WITHDREW_FROM_ACCOUNT: 'withdrew from account',
-    DEPOSITED_INTO_ACCOUNT: 'deposited into account'
+    DEPOSITED_INTO_ACCOUNT: 'deposited into account',
+	LOGIN_USER: 'login user',
+	SIGNUP_USER: 'signup user',
+	CURRENT_USER: 'authenticated user'
 };
