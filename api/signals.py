fca3cb645adde116676757b75834e3e55705204a
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-04-04 14:25:30 +0200

    Update signals.py

--- api/signals.py
+++ api/signals.py
@@ -2,6 +2,8 @@ from django.db.models.signals import post_save
 from django.contrib.auth.models import User
 from django.dispatch import receiver
 
+from api.models import Account
+
 from rest_framework.authtoken.models import Token
 
 @receiver(post_save, sender=User)
@@ -12,3 +14,20 @@ def init_new_user(sender, instance, signal, created, **kwargs):
     if created:
         print('Token created')
         Token.objects.create(user=instance)
+		
+
+@receiver(post_save, sender=User)
+def create_user_account(sender, instance, created, **kwargs):
+	"""
+	Create an balance account on signup
+	"""
+    if created:
+        Account.objects.create(user=instance)
+
+
+@receiver(post_save, sender=User)
+def save_user_account(sender, instance, **kwargs):
+	"""
+	save the account in db
+	"""
+    instance.account.save()
