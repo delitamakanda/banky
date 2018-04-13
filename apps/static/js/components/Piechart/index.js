b02758dbf23ca582b96fa3411e79f39ce83f9320
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-04-13 15:11:08 +0200

    Update index.js

--- apps/static/js/components/Piechart/index.js
+++ apps/static/js/components/Piechart/index.js
@@ -1,15 +1,34 @@
 import React, { Component } from 'react';
- 
+import d3 from 'd3';
 import { LabeledArc } from './Arc';
  
 class Piechart extends Component {
     constructor() {
+		super();
+ 
+        this.pie = d3.layout.pie().value((d) => d.value);
+        this.colors = d3.scale.category10();
     }
  
     arcGenerator(d, i) {
+		return (
+            <LabeledArc key={`arc-${i}`}
+                        data={d}
+                        innerRadius={this.props.innerRadius}
+                        outerRadius={this.props.outerRadius}
+                        color={this.colors(i)} />
+        );
     }
  
     render() {
+		let pie = this.pie(this.props.data);
+		let translate = `translate(${this.props.x}, ${this.props.y})`;
+
+				return (
+					<g transform={translate}>
+						{pie.map((d, i) => this.arcGenerator(d, i))}
+					</g>
+				)
     }
 }
  
