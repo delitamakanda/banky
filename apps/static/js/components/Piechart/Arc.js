8147bdadde5eaf0c4788c74ec62dce9a544d1c5d
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-04-13 15:11:45 +0200

    Update Arc.js

--- apps/static/js/components/Piechart/Arc.js
+++ apps/static/js/components/Piechart/Arc.js
@@ -1,1 +1,23 @@
 
+import React, { Component } from 'react';
+import d3 from 'd3';
+ 
+class Arc extends Component {
+    constructor() {
+        super();
+    }
+ 
+    componentWillMount() {
+        this.updateD3(this.props);
+    }1
+ 
+    componentWillReceiveProps(newProps) {
+        this.updateD3(newProps);
+    }
+ 
+    updateD3(newProps) {
+    }
+ 
+    render() {
+    }
+}
