a8a0bde4e7a2ab6a15efb1db78db2c6357bf1dd4
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-04-13 15:04:29 +0200

    Create index.js

---
+++ apps/static/js/components/Piechart/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+ 
+import { LabeledArc } from './Arc';
+ 
+class Piechart extends Component {
+    constructor() {
+    }
+ 
+    arcGenerator(d, i) {
+    }
+ 
+    render() {
+    }
+}
+ 
+export default Piechart;
