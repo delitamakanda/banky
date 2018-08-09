32379af67553e4bf1ffeb95a78cf9345dda0e721
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-08-09 13:25:36 +0200

    Update constants.js

--- apps/static/js/constants.js
+++ apps/static/js/constants.js
@@ -5,5 +5,14 @@ export default {
 	LOGIN_USER: 'login user',
 	SIGNUP_USER: 'signup user',
 	CURRENT_USER: 'authenticated user',
-    LOGOUT_USER: 'logout user'
+    LOGOUT_USER: 'logout user',
+	ACTION_TYPE: {
+    	UPDATE_TRANSACTIONS: 'UPDATE_TRANSACTIONS',
+    	UPDATE_CATEGORIES: 'UPDATE_CATEGORIES',
+        UPDATE_RECETTES_DEPENSES: 'UPDATE_RECETTES_DEPENSES'
+    },
+    SWITCH_CASE: {
+        RECETTES: 'RECETTES',
+        DEPENSES: 'DEPENSES'
+    }
 };
