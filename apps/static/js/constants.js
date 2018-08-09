a2e0f8c88ec1abe6d62422d7b09ce8b3f70cd263
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-08-09 13:26:36 +0200

    error indentation

--- apps/static/js/constants.js
+++ apps/static/js/constants.js
@@ -1,18 +1,18 @@
 export default {
-    CREATED_ACCOUNT: 'created account',
-    WITHDREW_FROM_ACCOUNT: 'withdrew from account',
-    DEPOSITED_INTO_ACCOUNT: 'deposited into account',
+	CREATED_ACCOUNT: 'created account',
+	WITHDREW_FROM_ACCOUNT: 'withdrew from account',
+	DEPOSITED_INTO_ACCOUNT: 'deposited into account',
 	LOGIN_USER: 'login user',
 	SIGNUP_USER: 'signup user',
 	CURRENT_USER: 'authenticated user',
-    LOGOUT_USER: 'logout user',
+	LOGOUT_USER: 'logout user',
 	ACTION_TYPE: {
-    	UPDATE_TRANSACTIONS: 'UPDATE_TRANSACTIONS',
-    	UPDATE_CATEGORIES: 'UPDATE_CATEGORIES',
-        UPDATE_RECETTES_DEPENSES: 'UPDATE_RECETTES_DEPENSES'
-    },
-    SWITCH_CASE: {
-        RECETTES: 'RECETTES',
-        DEPENSES: 'DEPENSES'
-    }
+		UPDATE_TRANSACTIONS: 'UPDATE_TRANSACTIONS',
+		UPDATE_CATEGORIES: 'UPDATE_CATEGORIES',
+		UPDATE_RECETTES_DEPENSES: 'UPDATE_RECETTES_DEPENSES'
+	},
+	SWITCH_CASE: {
+		RECETTES: 'RECETTES',
+		DEPENSES: 'DEPENSES'
+	}
 };
