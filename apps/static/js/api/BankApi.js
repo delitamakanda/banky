a5a9bc8af56d90041225dea899fc664bc6869cfb
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-09-17 11:30:12 +0200

    Update BankApi.js

--- apps/static/js/api/BankApi.js
+++ apps/static/js/api/BankApi.js
@@ -1,9 +1,12 @@
 import 'whatwg-fetch';
 import 'babel-polyfill';
 
+import { csrftoken } from './utils';
+
 const API_URL = 'http://localhost:8000/api';
 const API_HEADERS = {
     'Content-Type': 'application/json',
+	'X-CSRFToken': csrftoken,
     mode: 'opaque'
 };
 
