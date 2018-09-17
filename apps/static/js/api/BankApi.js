0d9fd3488cea48fd4a3e9a1bbd9281665a0af5a3
Délita Makanda <delitamakanda@users.noreply.github.com>
2018-09-17 10:11:53 +0200

    Create BankApi.js

---
+++ apps/static/js/api/BankApi.js
@@ -0,0 +1,16 @@
+import 'whatwg-fetch';
+import 'babel-polyfill';
+
+const API_URL = 'http://localhost:8000/api';
+const API_HEADERS = {
+    'Content-Type': 'application/json',
+    Authorization: 'connard',
+    mode: 'opaque'
+};
+
+
+let export default BankAPI; = {
+
+};
+
+export default BankAPI;
