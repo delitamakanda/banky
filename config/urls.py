33caf64124bd7e64d521fcc559b47ccdfe4fdaea
Délita Makanda <delita.makanda@gmail.com>
2020-06-20 23:12:58 +0200

    feat: add static url

--- config/urls.py
+++ config/urls.py
@@ -27,8 +27,6 @@ urlpatterns = [
     url(r'^$', TemplateView.as_view(template_name='index.html')),
 ]
 
-# if settings.DEBUG:
-    # urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-    # urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-
-# urlpatterns += [url(r'.*', TemplateView.as_view(template_name='index.html'))]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
