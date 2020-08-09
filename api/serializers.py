d75790759c143c92e7d1c063f7d7683cba87fe3e
Délita Makanda <delita.makanda@gmail.com>
2020-08-09 22:39:54 +0200

    refactor: delete some useless code

--- api/serializers.py
+++ api/serializers.py
@@ -71,7 +71,6 @@ class UserSerializer(serializers.ModelSerializer):
             'first_name',
             'last_name',
             'email',
-            'password',
-            # 'auth_token',
+            # 'password',
             'date_joined',
         )
