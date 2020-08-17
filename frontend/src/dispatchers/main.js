0cc0f94f56039950e89d33ce17d8639530a3f990
Délita Makanda <delita.makanda@gmail.com>
2020-08-17 02:04:23 +0200

    chore: debug dispatch

--- frontend/src/dispatchers/main.js
+++ frontend/src/dispatchers/main.js
@@ -2,14 +2,14 @@ import { Dispatcher } from 'flux';
 import 'babel-polyfill';
 
 class AppDispatcher extends Dispatcher {
-    // dispatch(action = {}) {
-    //     console.log("Dispatched", action);
-    //     super.dispatch(action);
-    // }
+    dispatch(action = {}) {
+        console.log("Dispatched", action);
+        super.dispatch(action);
+    }
     /**
     Dispatches three actions for an async operation represented by promise.
     */
-   dispatchAsync(promise, types, payload) {
+    dispatchAsync(promise, types, payload) {
         const { request, success, failure } = types;
         this.dispatch({ type: request, payload: Object.assign({}, payload) });
         promise.then(
