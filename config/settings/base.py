a9eb0ab66d74c44bc4b117a474b5797f0e058f0d
Délita Makanda <delita.makanda@gmail.com>
2023-07-10 17:21:15 +0200

    feat(openapi): add drf yasg

--- config/settings/base.py
+++ config/settings/base.py
@@ -45,6 +45,7 @@ THIRD_PARTY_APPS = [
     'rest_framework',
     'rest_framework.authtoken',
     'corsheaders',
+    'drf_yasg',
 ]
 
 PROJECT_APPS = [
--- config/urls.py
+++ config/urls.py
@@ -14,16 +14,31 @@ Including another URLconf
     2. Add a URL to urlpatterns:  url(r'^blog/', include('blog.urls'))
 """
 from django.conf import settings
-from django.urls import path, include
+from django.urls import path, include, re_path
 from django.contrib import admin
 from django.views.generic.base import TemplateView
 from rest_framework.schemas import get_schema_view
 from django.conf.urls.static import static
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Bank API",
+      default_version='v2.0',
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
     path('', TemplateView.as_view(template_name='index.html')),
 ]
 
