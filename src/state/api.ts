48b95d97353db5b5e56ef3b0b3fdce4346393e54
Délita Makanda <delita.makanda@gmail.com>
2023-07-11 20:34:24 +0200

    feat(api): fetch products and transactions

--- src/state/api.ts
+++ src/state/api.ts
@@ -1,14 +1,22 @@
 import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react';
-import { GetKpisResponse } from './types';
+import { GetKpisResponse, GetTransactionsResponse, GetProductsResponse } from './types';
 
 export const api = createApi({
     baseQuery: fetchBaseQuery({baseUrl: import.meta.env.VITE_BASE_URL}),
     reducerPath: 'main',
-    tagTypes: ["Kpis"],
+    tagTypes: ["Kpis", "Products", "Transactions"],
     endpoints: (build) => ({
         getKpis: build.query<Array<GetKpisResponse>, void>({
             query: () => "/api/keys-performance-indicators/",
             providesTags: ["Kpis"]
+        }),
+        getProducts: build.query<Array<GetProductsResponse>, void>({
+            query: () => "/api/products/",
+            providesTags: ["Products"]
+        }),
+        getTransactions: build.query<Array<GetTransactionsResponse>, void>({
+            query: () => "/api/products/",
+            providesTags: ["Transactions"]
         })
     })
 });
--- src/state/types.ts
+++ src/state/types.ts
@@ -6,15 +6,27 @@ export interface GetKpisResponse {
     /**
      * Expenses by category
      */
-    expenses_by_category?: any;
+    expenses_by_category: Map<string, string>;
     /**
      * Daily data
      */
-    daily_data?: any;
+    daily_data: DailyData[];
     /**
      * Monthly data
      */
     monthly_data: MonthlyData[];
+}
+export interface GetProductsResponse {
+    readonly id?: string;
+    price: string;
+    expense: string;
+    transactions?: Array<string>;
+}
+export interface GetTransactionsResponse {
+    readonly id?: string;
+    amount: string;
+    buyer: number;
+    products_ids?: Array<string>;
 }
 
 export interface MonthlyData {
@@ -23,4 +35,73 @@ export interface MonthlyData {
     nonOperationalExpenses: string;
     operationalExpenses: string;
     revenue: string;
+}
+
+export interface DailyData {
+    date: string;
+    expenses: string;
+    revenue: string;
+}
+
+export interface AuthToken { 
+    username: string;
+    password: string;
+    readonly token?: string;
+}
+
+export interface Action { 
+    readonly id?: number;
+    readonly user_friendly_id?: string;
+    /**
+     * User on make an action
+     */
+    user: number;
+    readonly created?: Date;
+    account: number;
+    type?: TypeEnum;
+    /**
+     * Balance delta.
+     */
+    delta: number;
+    reference: string;
+    reference_type?: ReferenceTypeEnum;
+    comment?: string;
+    /**
+     * Balance after action
+     */
+    debug_balance: number;
+}
+export type TypeEnum = 'CREATED' | 'DEPOSITED' | 'WITHDRAWN';
+export const TypeEnum = {
+    CREATED: 'CREATED' as TypeEnum,
+    DEPOSITED: 'DEPOSITED' as TypeEnum,
+    WITHDRAWN: 'WITHDRAWN' as TypeEnum
+};
+export type ReferenceTypeEnum = 'BANK_TRANSFER' | 'CHECK' | 'CASH' | 'NONE';
+export const ReferenceTypeEnum = {
+    BANKTRANSFER: 'BANK_TRANSFER' as ReferenceTypeEnum,
+    CHECK: 'CHECK' as ReferenceTypeEnum,
+    CASH: 'CASH' as ReferenceTypeEnum,
+    NONE: 'NONE' as ReferenceTypeEnum
+};
+
+export interface Account { 
+    readonly id?: number;
+    readonly uid?: string;
+    user: number;
+    readonly created?: Date;
+    readonly modified?: Date;
+    balance?: number;
+    readonly days_since_created?: string;
+}
+
+export interface User { 
+    readonly id?: number;
+    username: string;
+    first_name?: string;
+    last_name?: string;
+    email: string;
+    password: string;
+    date_joined?: Date;
+    password2: string;
 }
