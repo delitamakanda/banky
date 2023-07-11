5c3c3bd74a320969063169d4dcf91f4a816ea5f3
Délita Makanda <delita.makanda@gmail.com>
2023-07-11 22:53:58 +0200

    feat(transactions): fetch transactions #wip

--- src/scenes/dashboard/Row2.tsx
+++ src/scenes/dashboard/Row2.tsx
@@ -1,13 +1,19 @@
 import BoxHeader from "@/components/BoxHeader";
 import DashboardBox from "@/components/DashboardBox";
+import FlexBetween from "@/components/FlexBetween";
 import { useGetKpisQuery, useGetProductsQuery } from "@/state/api";
-import { useTheme } from "@mui/material";
+import { Box, Typography, useTheme } from "@mui/material";
 import { useMemo } from "react";
-import { ResponsiveContainer, LineChart, CartesianGrid, XAxis, Tooltip, YAxis, Legend, Line } from "recharts";
+import { ResponsiveContainer, LineChart, ScatterChart, Scatter, CartesianGrid, XAxis, Tooltip, YAxis, Line, Cell, PieChart, Pie, ZAxis } from "recharts";
 
 
 const Row2 = () => {
     const { palette } = useTheme()
+    const data = [
+        { name: 'Group A', value: 600 },
+        { name: 'Group B', value: 400 },
+    ];
+    const COLORS = [palette.primary[800], palette.primary[300]];
     const { data: operationalData } = useGetKpisQuery();
     const { data: productData } = useGetProductsQuery();
     console.log('data:', operationalData);
@@ -22,6 +28,17 @@ const Row2 = () => {
             })
         )
     }, [operationalData])
+    const productExpenseData = useMemo(() => {
+        return (
+            productData && productData.map(({ id, expense, price }) => {
+                return {
+                    id,
+                    expense,
+                    price
+                }
+            })
+        )
+    }, [productData])
     return (
         <>
         <DashboardBox gridArea="d">
@@ -46,8 +63,56 @@ const Row2 = () => {
                 </LineChart>
             </ResponsiveContainer>
         </DashboardBox>
-        <DashboardBox gridArea="e"></DashboardBox>
-        <DashboardBox gridArea="f"></DashboardBox>
+        <DashboardBox gridArea="e">
+            <BoxHeader progression="+5%" title="Campaigns and Targets" />
+            <FlexBetween>
+                <PieChart width={110} height={100} margin={{top: 0, right: -10, left: 10, bottom: 0}}>
+                <Pie
+                stroke="none"
+                data={data}
+                innerRadius={18}
+                outerRadius={38}
+                paddingAngle={2}
+                dataKey="value"
+                >
+                {data.map((entry, index) => (
+                    <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
+                ))}
+                </Pie>
+                </PieChart>
+                <Box ml="-0.7rem" flexBasis="40%" textAlign="center">
+                    <Typography variant="h5">Target Sales</Typography>
+                    <Typography m="0.3rem 0" variant="h3" color={palette.primary[300]}>83</Typography>
+                    <Typography variant="h6">Finance goals of the campaign that is desired</Typography>
+                </Box>
+                <Box flexBasis="40%">
+                    <Typography variant="h5">Losses in Revenue</Typography>
+                    <Typography variant="h6">Losses are down 25%</Typography>
+                    <Typography variant="h5" mt="0.4rem">Profit Margins</Typography>
+                    <Typography variant="h6">Margins are up by 30% from last month.</Typography>
+                </Box>
+            </FlexBetween>
+        </DashboardBox>
+        <DashboardBox gridArea="f">
+            <BoxHeader progression="+5%" title="Product Prices vs Expenses" />
+            <ResponsiveContainer width="100%" height="100%">
+                <ScatterChart
+                margin={{
+                    top: 20,
+                    right: 25,
+                    bottom: 40,
+                    left: -10,
+                }}
+                >
+                <CartesianGrid stroke={palette.grey[800]} />
+                <XAxis type="number" dataKey="price" name="price" tickLine={false} axisLine={false}  style={{ fontSize: '10px'}} tickFormatter={(v) => `$${v}`} />
+                <YAxis type="number" dataKey="expense" name="expense" tickLine={false} axisLine={false}  style={{ fontSize: '10px'}}  tickFormatter={(v) => `$${v}`}  />
+                <ZAxis type="number" range={[20]} />
+                <Tooltip formatter={(v) => `$${v}`} />
+                <Scatter name="Product Expense Ratio" data={productExpenseData} fill={palette.tertiary[500]} />
+                </ScatterChart>
+            </ResponsiveContainer>
+        </DashboardBox>
         </>
     )
 }
--- src/scenes/dashboard/Row3.tsx
+++ src/scenes/dashboard/Row3.tsx
@@ -1,19 +1,32 @@
+import BoxHeader from "@/components/BoxHeader";
 import DashboardBox from "@/components/DashboardBox";
-import { useGetTransactionsQuery } from "@/state/api";
+import { useGetTransactionsQuery, useGetProductsQuery } from "@/state/api";
 import { useTheme } from "@mui/material";
 
 type Props = {};
 
 const Row3 = (props: Props) => {
     const { palette } = useTheme()
-    const { data } = useGetTransactionsQuery();
-    console.log('data: ', data);
+    const { data: transactionData } = useGetTransactionsQuery();
+    const { data: productData } = useGetProductsQuery();
+    console.log('data: ', productData);
+    console.log('data: ', transactionData);
+    const countProducts = productData && productData.length || 0;
+    const countTransactions = transactionData && transactionData.length || 0;
     return (
         <>
-            <DashboardBox gridArea="g"></DashboardBox>
-            <DashboardBox gridArea="h"></DashboardBox>
-            <DashboardBox gridArea="i"></DashboardBox>
-            <DashboardBox gridArea="j"></DashboardBox>
+            <DashboardBox gridArea="g">
+                <BoxHeader progression={`${countProducts} products`} title="List of Products" />
+            </DashboardBox>
+            <DashboardBox gridArea="h">
+                <BoxHeader progression={`${countTransactions} latest transactions`} title="Recent Orders" />
+            </DashboardBox>
+            <DashboardBox gridArea="i">
+                <BoxHeader progression="+5%" title="Expense Breakdown By Category" />
+            </DashboardBox>
+            <DashboardBox gridArea="j">
+                <BoxHeader progression="+15%" title="Overall Summary and Explanation Data" />
+            </DashboardBox>
         </>
     )
 }
